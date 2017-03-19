b3eaf0f1088bee52a1b6232bbb1bb50dab16aed0
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-03-19 21:14:24 +0100

    add UPNP errors

--- UpnpLibrary/upnperror.cpp
+++ UpnpLibrary/upnperror.cpp
@@ -6,6 +6,8 @@ UpnpError::UpnpError(QNetworkReply::NetworkError netError, QByteArray data, QObj
 {
     m_data.setContent(data);
 
+    qWarning() << "UPNPERROR RECEIVED" << data;
+
     // read UPNP error
     QDomElement body = m_data.documentElement().firstChildElement("s:Body");
     if (!body.isNull())
@@ -42,6 +44,114 @@ UpnpError::UpnpError(QNetworkReply::NetworkError netError, QByteArray data, QObj
 
     if (faultString() != "UPnPError")
         qCritical() << "invalid faultString" << faultString();
+}
+
+UpnpError::UpnpError(ErrorTypes type, QObject *parent):
+    QObject(parent),
+    m_netError(QNetworkReply::InternalServerError)
+{
+    m_data.appendChild(m_data.createProcessingInstruction("xml", "version=\"1.0\""));
+
+    QDomElement envelope = m_data.createElementNS("http://schemas.xmlsoap.org/soap/envelope/", "s:Envelope");
+    envelope.setAttribute("s:encodingStyle", "http://schemas.xmlsoap.org/soap/encoding/");
+    m_data.appendChild(envelope);
+
+    QDomElement body = m_data.createElement("s:Body");
+    envelope.appendChild(body);
+
+    m_fault = m_data.createElement("s:Fault");
+    body.appendChild(m_fault);
+
+    QDomElement faultCode = m_data.createElement("faultcode");
+    faultCode.appendChild(m_data.createTextNode("s:Client"));
+    m_fault.appendChild(faultCode);
+
+    QDomElement faultString = m_data.createElement("faultstring");
+    faultString.appendChild(m_data.createTextNode("UPnPError"));
+    m_fault.appendChild(faultString);
+
+    QDomElement detail = m_data.createElement("detail");
+    m_fault.appendChild(detail);
+
+    m_detail = m_data.createElementNS("urn:schemas-upnp-org:control-1-0", "UPnPError");
+    detail.appendChild(m_detail);
+
+    QDomElement errorCode = m_data.createElement("errorCode");
+    QDomElement errorDescription = m_data.createElement("errorDescription");
+
+    errorCode.appendChild(m_data.createTextNode(QString("%1").arg(type)));
+
+    if (type == INVALID_ACTION)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Invalid Action"));
+    }
+    else if (type == INVALID_ARGS)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Invalid Args"));
+    }
+    else if (type == ACTION_FAILED)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Action Failed"));
+    }
+    else if (type == ARGUMENT_VALUE_INVALID)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Argument Value Invalid"));
+    }
+    else if (type == ARGUMENT_VALUE_OUT_OF_RANGE)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Argument Value Out of Range"));
+    }
+    else if (type == OPTIONAL_ACTION_NOT_IMPLEMENTED)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Optional Action Not Implemented"));
+    }
+    else if (type == OUT_OF_MEMORY)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Out of Memory"));
+    }
+    else if (type == HUMAN_INTERVENTION_REQUIRED)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Human Intervention Required"));
+    }
+    else if (type == STRING_ARGUMENT_TOO_LONG)
+    {
+        errorDescription.appendChild(m_data.createTextNode("String Argument Too Long"));
+    }
+    else if (type == ACTION_NOT_AUTHORIZED)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Action not authorized"));
+    }
+    else if (type == SIGNATURE_FAILED)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Signature failure"));
+    }
+    else if (type == SIGNATURE_MISSING)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Signature missing"));
+    }
+    else if (type == NOT_ENCRYPTED)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Not encrypted"));
+    }
+    else if (type == INVALID_SEQUENCE)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Invalid sequence"));
+    }
+    else if (type == INVALID_CONTROL_URL)
+    {
+        errorDescription.appendChild(m_data.createTextNode("Invalid control URL"));
+    }
+    else if (type == NO_SUCH_SESSION)
+    {
+        errorDescription.appendChild(m_data.createTextNode("No such session"));
+    }
+    else
+    {
+        qCritical() << "invalid type" << type;
+    }
+
+    m_detail.appendChild(errorCode);
+    m_detail.appendChild(errorDescription);
 }
 
 QNetworkReply::NetworkError UpnpError::netError() const
@@ -99,4 +209,9 @@ QString UpnpError::description() const
     {
         return QString();
     }
+}
+
+QByteArray UpnpError::toByteArray(const int indent) const
+{
+    return m_data.toByteArray(indent);
 }
--- UpnpLibrary/upnperror.h
+++ UpnpLibrary/upnperror.h
@@ -11,7 +11,34 @@ class UpnpError : public QObject
     Q_OBJECT
 
 public:
+    enum ErrorTypes { INVALID_ACTION = 401, // No action by that name at this service.
+                      INVALID_ARGS,         // Could be any of the following: not enough in args, no in arg by that name,
+                                            // one or more in args are of the wrong data type.
+                                            // Additionally, the UPnP Certification Test Tool Shall return the following warning message if there are too many in args:
+                                            // ‘Sending too many in args is not recommended and may cause unexpected results’
+
+                      ACTION_FAILED = 501,  // May be returned in current state of service prevents invoking that action.
+
+                      ARGUMENT_VALUE_INVALID = 600,       // The argument value is invalid
+                      ARGUMENT_VALUE_OUT_OF_RANGE,  // An argument value is less than the minimum or more than the maximum value of the allowedValueRange, or is not in the allowedValueList.
+                      OPTIONAL_ACTION_NOT_IMPLEMENTED,   // The requested action is optional and is not implemented by the device.
+                      OUT_OF_MEMORY,   // The device does not have sufficient memory available to complete the action.
+                                       // This may be a temporary condition; the control point may choose to retry the unmodified request again later and it may succeed if memory is available.
+                      HUMAN_INTERVENTION_REQUIRED,  // The device has encountered an error condition which it cannot resolve itself and required human intervention such as a reset or power cycle.
+                                                    // See the device display or documentation for further guidance.
+                      STRING_ARGUMENT_TOO_LONG,  // A string argument is too long for the device to handle properly.
+                      ACTION_NOT_AUTHORIZED,     // The action requested requires authorization and the sender was not authorized.
+                      SIGNATURE_FAILED,          // The sender's signature failed to verify.
+                      SIGNATURE_MISSING,         // The action requested requires a digital signature and there was none provided.
+                      NOT_ENCRYPTED,             // This action requires confidentiality but the action was not delivered encrypted.
+                      INVALID_SEQUENCE,          // The <sequence> provided was not valid.
+                      INVALID_CONTROL_URL,       // The controlURL within the <freshness> element does not match the controlURL of the action actually invoked (or the controlURL in the HTTP header).
+                      NO_SUCH_SESSION,           // The session key reference is to a non-existent session.
+                                                 // This could be because the device has expired a session, in which case the control point needs to open a new one.
+                    };
+
     explicit UpnpError(QNetworkReply::NetworkError netError, QByteArray data, QObject *parent = 0);
+    explicit UpnpError(ErrorTypes type, QObject *parent = 0);
 
     QNetworkReply::NetworkError netError() const;
 
@@ -20,6 +47,8 @@ public:
 
     int code() const;
     QString description() const;
+
+    QByteArray toByteArray(const int indent = -1) const;
 
 signals:
 
