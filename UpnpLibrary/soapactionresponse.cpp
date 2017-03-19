30067a4475bc4ea0097771f04b0b77fd012e2260
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-03-19 21:13:08 +0100

    SoapActionResponse to provide answer to UPNP SoapAction

--- UpnpLibrary/soapactionresponse.cpp
+++ UpnpLibrary/soapactionresponse.cpp
@@ -1,6 +1,57 @@
 #include "soapactionresponse.h"
 
-SoapActionResponse::SoapActionResponse(QObject *parent) : QObject(parent)
+SoapActionResponse::SoapActionResponse(QString serviceType, QString actionName, QObject *parent):
+    QObject(parent),
+    m_valid(true),
+    m_serviceType(serviceType),
+    m_actionName(actionName)
 {
+    m_xml.appendChild(m_xml.createProcessingInstruction("xml", "version=\"1.0\""));
 
+    QDomElement envelope = m_xml.createElementNS("http://schemas.xmlsoap.org/soap/envelope/", "s:Envelope");
+    envelope.setAttribute("s:encodingStyle", "http://schemas.xmlsoap.org/soap/encoding/");
+    m_xml.appendChild(envelope);
+
+    QDomElement body = m_xml.createElement("s:Body");
+    envelope.appendChild(body);
+
+    m_xmlAction = m_xml.createElementNS(serviceType, "u:"+actionName+"Response");
+    body.appendChild(m_xmlAction);
 }
+
+bool SoapActionResponse::isValid() const
+{
+    return m_valid;
+}
+
+QString SoapActionResponse::serviceType() const
+{
+    return m_serviceType;
+}
+
+QString SoapActionResponse::actionName() const
+{
+    return m_actionName;
+}
+
+bool SoapActionResponse::addArgument(const QString &name, const QString &value)
+{
+    if (!m_xmlAction.elementsByTagName(name).isEmpty())
+    {
+        qCritical() << "argument" << name << "already exists.";
+        return false;
+    }
+    else
+    {
+        QDomElement arg = m_xml.createElement(name);
+        arg.appendChild(m_xml.createTextNode(value));
+        m_xmlAction.appendChild(arg);
+        return true;
+    }
+}
+
+QByteArray SoapActionResponse::toByteArray() const
+{
+    return m_xml.toByteArray(-1);
+}
+
--- UpnpLibrary/soapactionresponse.h
+++ UpnpLibrary/soapactionresponse.h
@@ -2,16 +2,36 @@
 #define SOAPACTIONRESPONSE_H
 
 #include <QObject>
+#include <QHash>
+#include <QDebug>
+#include <QDomDocument>
 
 class SoapActionResponse : public QObject
 {
     Q_OBJECT
+
 public:
-    explicit SoapActionResponse(QObject *parent = 0);
+    explicit SoapActionResponse(QString serviceType, QString actionName, QObject *parent = 0);
+
+    bool isValid() const;
+
+    QString serviceType() const;
+    QString actionName() const;
+
+    bool addArgument(const QString &name, const QString &value);
+
+    QByteArray toByteArray() const;
 
 signals:
 
 public slots:
+
+private:
+    bool m_valid;
+    QString m_serviceType;
+    QString m_actionName;
+    QDomDocument m_xml;
+    QDomElement m_xmlAction;
 };
 
 #endif // SOAPACTIONRESPONSE_H
