daa07795e6199af7ba63d7f15dca8343ad847b52
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2017-04-19 20:39:48 +0200

    new class StateVariableItem

--- UpnpLibrary/statevariableitem.cpp
+++ UpnpLibrary/statevariableitem.cpp
@@ -1,6 +1,51 @@
 #include "statevariableitem.h"
 
-StateVariableItem::StateVariableItem(QObject *parent) : QObject(parent)
+StateVariableItem::StateVariableItem(QObject *parent):
+    ListItem(parent)
 {
+    m_roles[NameRole] = "name";
+    m_roles[ValueRole] = "value";
+}
 
+QHash<int, QByteArray>  StateVariableItem::roleNames() const
+{
+    return m_roles;
+}
+
+QVariant StateVariableItem::data(int role) const
+{
+    switch (role) {
+    case NameRole:
+        return m_name;
+    case ValueRole:
+        return m_value;
+    default:
+        break;
+    }
+    return QVariant::Invalid;
+}
+
+bool StateVariableItem::setData(const QVariant &value, const int &role)
+{
+    QVector<int> roles;
+
+    switch (role)
+    {
+    case NameRole:
+        m_name = value.toString();
+        roles << role;
+        emit itemChanged(roles);
+        return true;
+
+    case ValueRole:
+        m_value = value.toString();
+        roles << role;
+        emit itemChanged(roles);
+        return true;
+
+    default:
+        qCritical() << this << "invalid role" << role;
+    }
+
+    return false;
 }
--- UpnpLibrary/statevariableitem.h
+++ UpnpLibrary/statevariableitem.h
@@ -1,17 +1,34 @@
 #ifndef STATEVARIABLEITEM_H
 #define STATEVARIABLEITEM_H
 
-#include <QObject>
+#include "Models/listitem.h"
+#include <QVariant>
+#include <QDebug>
 
-class StateVariableItem : public QObject
+class StateVariableItem : public ListItem
 {
     Q_OBJECT
+
 public:
+    enum Roles {
+        NameRole = Qt::UserRole+1,
+        ValueRole
+    };
+
     explicit StateVariableItem(QObject *parent = 0);
+
+    virtual QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
+    virtual QVariant data(int role) const Q_DECL_OVERRIDE;
+    virtual bool setData(const QVariant &value, const int &role) Q_DECL_OVERRIDE;
 
 signals:
 
 public slots:
+
+private:
+    QHash<int, QByteArray> m_roles;
+    QString m_name;
+    QString m_value;
 };
 
 #endif // STATEVARIABLEITEM_H
