b31c0dfe63ac49d830c75ddf76a0e87b6569b75e
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2018-06-17 17:56:38 +0200

    new class XmlEvent

--- UpnpLibrary/xmlevent.cpp
+++ UpnpLibrary/xmlevent.cpp
@@ -2,5 +2,22 @@
 
 XmlEvent::XmlEvent()
 {
+    m_xml.appendChild(m_xml.createProcessingInstruction("xml", "version=\"1.0\""));
+    m_xml.appendChild(m_xml.createElementNS("urn:schemas-upnp-org:event-1-0", "e:propertyset"));
+}
 
+QDomElement XmlEvent::addProperty(const QString &name, const QString &value)
+{
+    QDomElement propertySet = m_xml.firstChildElement("propertyset");
+    QDomElement property = m_xml.createElement("e:property");
+    propertySet.appendChild(property);
+    QDomElement param = m_xml.createElement(name);
+    param.appendChild(m_xml.createTextNode(value));
+    property.appendChild(param);
+    return property;
+}
+
+QString XmlEvent::toString() const
+{
+    return m_xml.toString();
 }
--- UpnpLibrary/xmlevent.h
+++ UpnpLibrary/xmlevent.h
@@ -1,11 +1,21 @@
 #ifndef XMLEVENT_H
 #define XMLEVENT_H
 
+#include <QDomDocument>
+#include <QDebug>
 
 class XmlEvent
 {
+
 public:
     XmlEvent();
+
+    QDomElement addProperty(const QString &name, const QString &value);
+
+    QString toString() const;
+
+private:
+    QDomDocument m_xml;
 };
 
 #endif // XMLEVENT_H
