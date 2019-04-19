4aa24e5e4f524c966d6e43b16e96d334a9b32fc8
HIMBERT Guillaume <guillaume.himbert@gmail.com>
2019-04-19 21:28:34 +0200

    minor improvement

--- UpnpLibrary/Http/httprequest.cpp
+++ UpnpLibrary/Http/httprequest.cpp
@@ -1042,7 +1042,7 @@ QString HttpRequest::errorString() const
 
 void HttpRequest::setError(const QString &message)
 {
-    qCritical() << message;
+    qCritical() << this << message;
     logMessage(QString("ERROR : %1.").arg(message));
     m_error = message;
     setData("KO", statusRole);
@@ -1170,12 +1170,8 @@ void HttpRequest::timerEvent(QTimerEvent *event)
             if (clockSending.isValid())
             {
                 // emit progress every 10 seconds
-                if (clockSending.elapsedFromBeginning()/1000 % 10 == 0)
+                if (clockSending.elapsedFromBeginning()>10000 && clockSending.elapsedFromBeginning()/1000 % 10 == 0)
                     emit servingSignal(m_requestedResource, clockSending.elapsedFromBeginning());
-            }
-            else
-            {
-                emit servingSignal(m_requestedResource, 0);
             }
 
             if (m_client)
